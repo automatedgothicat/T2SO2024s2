6f6eb439d5313e7260a65bbe079173b5ce060d79
automatedgothicat <solrachenrigit@gmail.com>
2024-11-05 15:49:31 -0400

    Copied Color Macros

--- main.cpp
+++ main.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+//************* MACROS MUDANCA COR DE TEXTO, ANSI ESCAPE CODES ****
+#define COR_RESET       "\033[0m"
+#define COR_VERDE       "\033[32m"
+#define COR_VERMELHO    "\033[31m"
+#define COR_AMARELO     "\033[38;5;11m"
+#define COR_MAGENTA     "\033[38;5;207m"
+#define COR_CIANO       "\033[36m"
+#define COR_MUSGO       "\033[38;5;64m"
+#define COR_LARANJADO   "\033[38;5;202m"
+#define COR_ROXO        "\033[38;5;91m"
+#define COR_MARROM      "\033[38;5;189m"
+#define COR_AZUL        "\033[38;5;39m"
+#define COR_BRANCO      "\033[38;5;15m"
+#define COR_MESA        "\033[38;5;101m"
+// para usar um texto de uma cor use: cout << COR_MUSGO << "O" << COR_RESET;
+
 #define nfilosofos 5
 int garfos[nfilosofos] = { -1,-1,-1,-1,-1 };
 int refeicoes[nfilosofos] = {0};
